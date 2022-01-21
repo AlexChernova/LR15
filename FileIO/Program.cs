290cb227dce5d90673f9f61aa69469dc38f601bc
Александра Чернова <sahsachernova2003@gmail.com>
2022-01-21 11:02:34 +0200

    first commit

---
+++ FileIO/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+namespace ConsoleApplication1
+{
+    class Files
+    {
+        static void Main()
+        {
+            var adress = File.Create(@"D:\Primer.txt");//создание файла
+            adress.Close();//закрытии файла и освобождение системных ресурсов
+            File.Move(@"D:\Primer.txt", "D:\\Primer.txt");//переместить файл в указанную папку
+            var adress2 = File.Create("Primer2.txt");//Помещается в директорию Debug
+            adress2.Close();
+            File.Delete("D:\\Primer.txt");//удаление файла
+            if (File.Exists("Primer2.txt"))//если файл2 существует
+            { File.Copy("Primer2.txt", "D:\\Primer2.txt", true); }//скопировать в указанную папку
+        }
+    }
+}
